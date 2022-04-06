032005bd499f8b041d84220a8f5a5cff6bfad973
unknown <adamavip@gmail.com>
2022-04-06 16:18:59 +0000

    Updating preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,8 @@
 import numpy as np
+import scipy
+import scipy.signal
+import scipy.io as io
+import scipy.ndimage as nd
 
 def msc(input_data, reference=None):
     ''' 
@@ -37,12 +41,12 @@ def snv(input_data):
     """
     Performs a Standard Normal Variate (SNV) correction.
 
-    :Args:
-        input_data: numpy.ndarray
-        reference: scalar
+    Args:
+        input_data <numpy.ndarray>: NIR data matrix
+        reference <float>: reference value to correct data 
     
-    :Returns:
-        corrected spectra: numpy.ndarray
+    Returns:
+        spectra <numpy.ndarray>: corrected spectra 
     
     """
     # Define a new array and populate it with the corrected data  
@@ -53,3 +57,42 @@ def snv(input_data):
         output_data[i,:] = (input_data[i,:] - np.mean(input_data[i,:])) / np.std(input_data[i,:])
  
     return output_data
+
+
+def smooth(spectra, filter_win=11, window_type='flat', mode='reflect'):
+    """ Smooths the spectra using convolution.
+    Args:
+        spectra <numpy.ndarray>: NIRS data matrix.
+        filter_win <float>: length of the filter window in samples.
+        window_type <str>: filtering window to use for convolution (see scipy.signal.windows)
+        mode <str>: convolution mode
+    Returns:
+        spectra <numpy.ndarray>: Smoothed NIR spectra.
+    """
+
+    if window_type == 'flat':
+        window = np.ones(filter_win)
+    else:
+        window = scipy.signal.windows.get_window(window_type, filter_win)
+    window = window / np.sum(window)
+
+    for row in range(spectra.shape[0]):
+        spectra[row,:] = nd.convolve(spectra[row,:], window, mode=mode)
+
+    return spectra
+
+
+def savgol(spectra, filter_win=15, poly_order=2, deriv_order=0, delta=1.0):
+    """ Perform Savitzky–Golay filtering on the data (also calculates derivatives). This function is a wrapper for
+    scipy.signal.savgol_filter.
+
+    Args:
+        spectra <numpy.ndarray>: NIRS data matrix.
+        filter_win <int>: Size of the filter window in samples (default 11).
+        poly_order <int>: Order of the polynomial estimation (default 3).
+        deriv_order <int>: Order of the derivation (default 0).
+
+    Returns:
+        spectra <numpy.ndarray>: NIRS data smoothed with Savitzky-Golay filtering
+    """
+    return scipy.signal.savgol_filter(spectra, filter_win, poly_order, deriv_order, delta=delta, axis=1)
